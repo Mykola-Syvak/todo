f4b44bf2cb90feebd7008993e1a9e29586490d6b
Mykola Syvak <mykola.syvak@gmail.com>
2020-01-18 11:30:17 +0200

    new

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import SearchPanel from './components/search-panel';
 const App = () => {
      
      const todoData = [
-          { label: 'Drink Coffee', important: false, id: 1 },
+          { label: 'Drink Coffeee', important: false, id: 1 },
           { label: 'Make Awesome App', important: true, id: 2 },
           { label: 'Have a lunch', important: false, id: 3 }
      ];
