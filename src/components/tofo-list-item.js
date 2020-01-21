2e75c5b5f1e0a0ffbf6c4568ae354bef64b2a7e8
Mykola Syvak <mykola.syvak@gmail.com>
2020-01-21 18:38:30 +0200

    продолжаем 32й...

--- src/components/tofo-list-item.js
+++ src/components/tofo-list-item.js
@@ -9,10 +9,21 @@ const TodoListItem = ({ label, important = false }) => {
     }
     
     return (
-    <span className="todo-list-item"
-          style={style}>
-        { label }
-    </span>);
+    <span className="todo-list-item">
+        <span className="todo-list-item-label"
+            style={style}>
+            { label }
+        </span>
+        <button type="button"
+                className="btn btn-outline-success btn-sm">
+            <i className="fa fa-exclamation" />
+        </button>
+        <button type="button"
+                className="btn btn-outline-danger btn-sm">
+            <i className="fa fa-trash-o" />
+        </button>
+    </span>
+    );
 };
 
 export default TodoListItem;
