6e7c16e56142547db6b5b333a79d0dad2e42f642
Mykola Syvak <mykola.syvak@gmail.com>
2020-01-22 13:41:39 +0200

    34й урок

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -2,30 +2,33 @@ import React from 'react';
 
 import './todo-list-item.css';
 
-const TodoListItem = ({ label, important = false }) => {
-    
-    const style = {
-        color: important ? 'tomato' : 'black'
-    }
-    
-    return (
-    <span className="todo-list-item">
-        <span className="todo-list-item-label"
-            style={style}>
-            { label }
-        </span>
-        <span className="todo-list-button">
-            <button type="button"
-                    className="btn btn-outline-success btn-sm">
-                <i className="fa fa-exclamation" />
-            </button>
-            <button type="button"
-                    className="btn btn-outline-danger btn-sm">
-                <i className="fa fa-trash-o" />
-            </button>
-        </span>
-    </span>
-    );
-};
+export default class TodoListItem extends React.Component {
 
-export default TodoListItem;
+    render() {
+
+        const { label, important = false } = this.props;
+
+        const style = {
+            color: important ? 'tomato' : 'black'
+        }
+        
+        return (
+        <span className="todo-list-item">
+            <span className="todo-list-item-label"
+                style={style}>
+                { label }
+            </span>
+            <span className="todo-list-button">
+                <button type="button"
+                        className="btn btn-outline-success btn-sm">
+                    <i className="fa fa-exclamation" />
+                </button>
+                <button type="button"
+                        className="btn btn-outline-danger btn-sm">
+                    <i className="fa fa-trash-o" />
+                </button>
+            </span>
+        </span>
+        );
+    }
+};
